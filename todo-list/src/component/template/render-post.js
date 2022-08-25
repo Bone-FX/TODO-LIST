55db6e97333d49a1f58b13a4b106e1bab36fd528
Evgenii <60846275+Bone-FX@users.noreply.github.com>
2022-08-25 18:53:48 +0300

    Add files via upload
    lastpush

---
+++ todo-list/src/component/template/render-post.js
@@ -0,0 +1,24 @@
+import {Storage} from "../../core/storage.js";
+
+export const renderTodos = () => {
+    const {todoList} = Storage.getUserData()
+
+    if (todoList.length === 0) {
+        return '<p class="todos__notice">List is Empty</p>'
+    }
+
+    return todoList.map(todo => {
+        const style = todo.status === 'done' ? 'todos__item todos__item_done' : 'todos__item'
+
+        return `
+        <div class="${style}" data-todo-id="${todo.id}" data-todo-status="${todo.status}">
+                      <div class="todos__item-status"></div>
+                <p class="todos__item-title">${todo.title}</p>
+            <div class="todos__item-edit"></div>
+            <div class="todos__item-remove"></div>
+        </div>
+        `
+    }).join(' ')
+}
+
+
